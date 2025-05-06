ae4f5c97e35c7df223223940fa9e49d136f5dd85
samratsiuri <samratsiuri@gmail.com>
2025-05-06 16:37:32 +0530

    Add files via upload

---
+++ a1t1.py
@@ -0,0 +1,9 @@
+#Task 1:Perform Basic mathematical Operation
+a=int(input('Enter The First Number: '))
+b=int(input('Enter The Second Number: '))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+
---
+++ a1task2.py
@@ -0,0 +1,4 @@
+#Task 2:Create A Personalised Greeting
+a=str(input("Enter Your First Name: "))
+b=str(input("Enter Your Last Name: "))
+print("Hello", a,b+"!"+"Welcome To The Python Program.")
